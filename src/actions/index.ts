ede744f9c7ebf781ca6aa3444d8e543041ca5a8e
e1 <e1x02@icloud.com>
2025-09-01 17:43:04 -0400

    i'm going to cry it was working and then i broke it trying to fix it but it's working again thank christ almighty.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -42,8 +42,8 @@ export const server = {
 		handler: async (input) => {
 			try {
 				console.log(JSON.stringify(input))
-				await db.insert(IGWC).values(input).onConflictDoUpdate({ target: IGWC.userID, set: input });
-				const googleScriptUrl = "https://script.google.com/macros/s/AKfycbxk17aA6djx2svd4hGZSdMLJZvFkAgUT1SDI4o5JHwlaaWcLZD1AuHX1YzpOY_wV4he/exec";
+				//await db.insert(IGWC).values(input).onConflictDoUpdate({ target: IGWC.userID, set: input });
+				const googleScriptUrl = "https://script.google.com/macros/s/AKfycby2oEQbkHixO7im5Ya2gOAUOATPWiypcHR9ZQlHz2adC77MZetEj5jGw_e7m_E9HLPqqQ/exec";
 				if (googleScriptUrl) {
 					
 					const response = await fetch(googleScriptUrl, {
